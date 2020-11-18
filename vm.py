95ad8d3d3acc3093d840e6f629c8ec1dd9b15df4
Xenxia <xen.devops@gmail.com>
2020-11-18 16:10:28 +0100

    :sparkles: Add command "destroy"

--- vm.py
+++ vm.py
@@ -26,20 +26,26 @@ import os, sys, argparse
 from os import system, name
 from time import sleep
 
-version="1.1.0"
+version="1.2.0"
 
 #? Color ------------------------------------------------------------------------------->
 class co:
     Reset = "\u001b[0m"+"\n"
 
-    Default      = "\u001b[0m"
-    Black        = "\u001b[38;5;0m"
-    Red          = "\u001b[38;5;160m"
-    Green        = "\u001b[38;5;82m"
-    Yellow       = "\u001b[38;5;226m"
-    Blue         = "\u001b[38;5;27m"
-    Magenta      = "\u001b[38;5;207m"
-    Cyan         = "\u001b[38;5;51m"
+    Default         = "\u001b[0m"
+    Black           = "\u001b[38;5;0m"
+    white           = "\u001b[38;5;255m"
+    Red             = "\u001b[38;5;160m"
+    Green           = "\u001b[38;5;82m"
+    Yellow          = "\u001b[38;5;226m"
+    Orange          = "\u001b[38;5;208m"
+    Blue            = "\u001b[38;5;27m"
+    Magenta         = "\u001b[38;5;207m"
+    Cyan            = "\u001b[38;5;51m"
+
+    Bold            = '\033[1m'
+    UnderLine       = '\033[4m'
+    Reverse         = "\033[;7m"
 
 #? Argument parser ------------------------------------------------------------------------------->
 parser = argparse.ArgumentParser(description="Application to merge the 'qm' and 'pct' commands from proxmox",
@@ -48,23 +54,27 @@ parser = argparse.ArgumentParser(description="Application to merge the 'qm' and 
                                 usage="%(prog)s"
                                 )
 
-subparsers = parser.add_subparsers(title ='Commande', 
+subparsers = parser.add_subparsers(title ='Commande',
                                   dest='Command_Name',
                                   )
 
-parser_start = subparsers.add_parser('start', help="{VMID} Start VM")
+parser_start = subparsers.add_parser('start', help="<VMID> Start VM")
 parser_start.add_argument('VMID', type=int, help='ID VM')
 
-parser_stop = subparsers.add_parser('stop', help='{VMID} Stop VM')
+parser_stop = subparsers.add_parser('stop', help='<VMID> Stop VM')
 parser_stop.add_argument('VMID', type=int, help='ID VM')
 
-parser_reboot = subparsers.add_parser('reboot', help='{VMID} Reboot VM')
+parser_reboot = subparsers.add_parser('reboot', help='<VMID> Reboot VM')
 parser_reboot.add_argument('VMID', type=int, help='ID VM')
 
-parser_console = subparsers.add_parser('console', help="{VMID} Open console vm 'for lxc vm'")
+parser_console = subparsers.add_parser('console', help="<VMID> Open console vm 'for lxc vm'")
 parser_console.add_argument('VMID', type=int, help='ID VM')
 
-group = parser.add_mutually_exclusive_group(required=False)
+parser_destroy = subparsers.add_parser('destroy', help="<VMID> [-purge] Destroy the container (also delete all uses files).")
+parser_destroy.add_argument('VMID', type=int, help='ID VM')
+parser_destroy.add_argument('-p', '-purge', action="store_true", dest='Purge', help='Remove container from all related configurations.')
+
+group = parser.add_mutually_exclusive_group()
 group.add_argument('-v', '--version', action='version', version=co.Green+version+co.Default)
 group.add_argument("-l", "--list", action="store_true", help ="shows the list of all VMs")
 
@@ -154,49 +164,44 @@ def Search_VM(ID_vm):
         for valeur in vm_dico[x].values():
             if valeur == str(ID_vm):
                 return vm_dico[x]
+            
+    print(co.Red+"No vm"+co.Reset)
+    Exit()
 
 def Start_VM(vm):
 
-    if vm == None:
-        print(co.Red+"No vm"+co.Reset)
+    if vm["Status"] == "running":
+        print(co.Magenta+"VM Already "+co.Green+"started "+co.Magenta+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
 
-    else:
-        if vm["Status"] == "running":
-            print(co.Magenta+"VM Already "+co.Green+"started "+co.Magenta+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
+    elif vm["Tag"] == "LXC":
+        print(co.Yellow+"Start Container "+vm["Name"]+':'+vm["VM_ID"]+co.Default)
+        os.system('pct start '+ str(vm["VM_ID"]))
+        print(co.Green+"Started Container "+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
 
-        elif vm["Tag"] == "LXC":
-            print(co.Yellow+"Start Container "+vm["Name"]+':'+vm["VM_ID"]+co.Default)
-            os.system('pct start '+ str(vm["VM_ID"]))
-            print(co.Green+"Started Container "+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
-
-        elif vm["Tag"] == "QM":
-            print(co.Yellow+"Start VM "+vm["Name"]+':'+vm["VM_ID"]+co.Default)
-            os.system('qm start '+ str(vm["VM_ID"]))
-            print(co.Green+"Started VM "+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
+    elif vm["Tag"] == "QM":
+        print(co.Yellow+"Start VM "+vm["Name"]+':'+vm["VM_ID"]+co.Default)
+        os.system('qm start '+ str(vm["VM_ID"]))
+        print(co.Green+"Started VM "+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
 
 def Stop_VM(vm):
 
-    if vm == None:
-        print(co.Red+"No vm"+co.Reset)
+    if vm["Status"] == "stopped":
+        print(co.Magenta+"VM Already "+co.Red+"stopped "+co.Magenta+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
 
-    else:
-        if vm["Status"] == "stopped":
-            print(co.Magenta+"VM Already "+co.Red+"stopped "+co.Magenta+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
+    elif vm["Tag"] == "LXC":
+        print(co.Yellow+"Stop Container "+vm["Name"]+':'+vm["VM_ID"]+co.Default)
+        os.system('pct stop '+ str(vm["VM_ID"]))
+        print(co.Green+"Stopped Container "+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
 
-        elif vm["Tag"] == "LXC":
-            print(co.Yellow+"Stop Container "+vm["Name"]+':'+vm["VM_ID"]+co.Default)
-            os.system('pct stop '+ str(vm["VM_ID"]))
-            print(co.Green+"Stopped Container "+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
-
-        elif vm["Tag"] == "QM":
-            print(co.Yellow+"Stop VM "+vm["Name"]+':'+vm["VM_ID"]+co.Default)
-            os.system('qm stop '+ str(vm["VM_ID"]))
-            print(co.Green+"Stopped VM "+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
+    elif vm["Tag"] == "QM":
+        print(co.Yellow+"Stop VM "+vm["Name"]+':'+vm["VM_ID"]+co.Default)
+        os.system('qm stop '+ str(vm["VM_ID"]))
+        print(co.Green+"Stopped VM "+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
 
 def Reboot_VM(vm):
 
-    if vm == None:
-        print(co.Red+"No vm"+co.Reset)
+    if vm["Status"] == "stopped":
+        print(co.Magenta+"The VM is "+co.Red+"stopped "+co.Magenta+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
 
     elif vm["Tag"] == "LXC":
         print(co.Yellow+"Reboot Container "+vm["Name"]+':'+vm["VM_ID"]+co.Default)
@@ -211,8 +216,8 @@ def Reboot_VM(vm):
 
 def Console_VM(vm):
 
-    if vm == None:
-        print(co.Red+"No vm"+co.Reset)
+    if vm["Status"] == "stopped":
+        print(co.Magenta+"The VM is "+co.Red+"stopped "+co.Magenta+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
 
     elif vm["Tag"] == "LXC":
         print(co.Yellow+"Exit the console Ctrl+a q"+co.Reset)
@@ -222,9 +227,38 @@ def Console_VM(vm):
     elif vm["Tag"] == "QM":
         print(co.Yellow+"use 'qm terminal' and config serial"+co.Reset)
 
+def Destroy_VM(vm, opti=""):
+
+    if vm["Status"] == "running":
+        print(co.Magenta+"The VM is "+co.Green+"running "+co.Magenta+vm["Name"]+':'+vm["VM_ID"]+co.Reset)
+
+    choiced_Action = input(co.Orange+"Do you really want to remove the VM : "+co.UnderLine+co.Cyan+vm["Name"]+co.Default+" ("+co.Green+"Y"+co.Default+"/"+co.Red+"[N]"+co.Default+"): ")
+    print()
+
+    if choiced_Action == 'Y' or choiced_Action == 'y' :
+
+        input_VMID = input(co.Magenta+"Please enter the ID to confirm"+co.Default+" ("+vm["VM_ID"]+") : ")
+
+        if input_VMID == vm["VM_ID"]:
+            print(co.Reset)
+            if vm["Tag"] == "LXC":
+                os.system('pct destroy '+ str(vm["VM_ID"])+' '+opti)
+
+            elif vm["Tag"] == "QM":
+                os.system('qm destroy '+ str(vm["VM_ID"])+' '+opti)
+
+        print()
+        Exit()
+
+    else:
+        Exit()
+
+
 #? Start ----------------------------------------------------------------------------------------->
 
 print()
+
+option = ""
 
 if args.Command_Name == 'start':
 
@@ -248,6 +282,13 @@ if args.Command_Name == 'console':
 
     vm_info = Search_VM(Check_IDVM(args.VMID))
     Console_VM(vm_info)
+    Exit()
+
+if args.Command_Name == 'destroy':
+
+    vm_info = Search_VM(Check_IDVM(args.VMID))
+    if args.Purge: option = "-purge "
+    Destroy_VM(vm_info, option)
     Exit()
 
 if args.list:
